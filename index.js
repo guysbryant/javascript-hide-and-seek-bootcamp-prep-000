02259d0b33b0cda1a03ede3e68be988df2cdcd85
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-26 18:55:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(nested, target){
+  return document.getElementById("nested").querySelector(".target");
+  
+}
+
+function increaseRankBy(n){
+  var ranked = document.querySelectorAll(".ranked-list li");
+  ranked.forEach(function(li){
+    li.innerHTML = parseInt(li.innerHTML) + n;
+  });
+}
+
+function deepestChild(){
+  var gNode = document.getElementById("grand-node");
+  function getChildren(element){
+    if (element.children.length > 0){
+      return getChildren(element.children[0])
+    } else {
+      return element
+    }
+  }
+  return getChildren(gNode);
+}
